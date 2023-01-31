bd6830efe6b48a7f6d40238e6fea63fc494a4954
Vatsal Shah <Vatsalshah4567@gmail.com>
2023-02-01 00:09:07 +0530

    small update

--- cloudtrade/urls.py
+++ cloudtrade/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('signup.html', views.signup, name='signup'),
     path('signin.html', views.signin, name='signin'),
     path('signout.html', views.signout, name='signout'),
+    path('home.html', views.home, name='home'),
 ]
--- cloudtrade/views.py
+++ cloudtrade/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+
 def home(request):
+    return render(request,'home.html')
+
+
+def index(request):
     return render(request,'index.html')
 
 def signup(request):
